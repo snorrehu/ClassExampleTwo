b7f28bf1718a63f30df0c9207043b01370ab4002
snorrehu <snorre.hukkelaas@gmail.com>
2018-09-13 13:14:06 +0200

    Add files via upload

--- CalcGUI.java
+++ CalcGUI.java
@@ -2,66 +2,90 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Calc extends JFrame{
-    private static String buttons[][] = {
-            {"7","8","9","+","*"},
-            {"4","5","6","-","ANS"},
-            {"1","2","3","/","="}
-    };
+public class Calculator extends JFrame{
+    private static String numberButtons[] = {"9","8","7","6","5","4","3","2","1"};
 
-    private static void main(String[] args){
-        Frame frame = new Frame();
+    private static String bottomButtons[] = {"0","ANS","=","CLEAR"};
+
+    private static String operatorButtons[] = {"-","/","*","+"};
+
+    public static void main(String[] args){
+        new Calculator();
     }
 
-
-    Calc() {
-        this.setTitle("Calculator");
+    Calculator() {
         this.setSize(300, 400);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setLayout(new BorderLayout());
 
+        //Displaypanel
+        JPanel displayPanel = new JPanel();
+        JTextArea displayArea = new JTextArea();
+        displayArea.setEditable(false);
+
+        Font f = new Font("Arial",Font.BOLD,14);
+        displayArea.setFont(f);
+
+        displayPanel.add(displayArea);
+
         //Create button listener
-        /*
         ActionListener listener = new ActionListener()
         {
             @Override
             public void actionPerformed(ActionEvent e)
             {
                 System.out.println(e.getActionCommand()+" clicked");
+                displayArea.setText(e.getActionCommand());
             }
-        };
-        */
 
-        //Buttonpanel
-        JPanel buttonPanel = new JPanel();
-        for(String[] row:buttons){
-            for(String elementInButtonMatrix:row){
-                //Create new button
-                JButton button = new JButton(elementInButtonMatrix);
-                //Set action command name for button
-                //button.setActionCommand(elementInButtonMatrix);
-                //Add listener
-                //button.addActionListener(listener);
-                //Add to panel
-                buttonPanel.add(button);
-            }
+        };
+
+        //Number button panel
+        JPanel numberButtonPanel = new JPanel(new GridLayout(3,3,4,4));
+        for(String element:numberButtons){
+            //Create new button
+            JButton button = new JButton(element);
+            //Set action command name for button
+            button.setActionCommand(element);
+            //Add listener
+            button.addActionListener(listener);
+            //Add to panel
+            numberButtonPanel.add(button);
         }
 
-        //Displaypanel
-        JPanel displayPanel = new JPanel();
-        JTextArea displayArea = new JTextArea();
-        displayArea.setEditable(false);
-        displayPanel.add(displayArea);
+        //Operator button panel
+        JPanel operatorButtonPanel = new JPanel(new GridLayout(4,1,4,4));
+        for(String element:operatorButtons){
+            //Create new button
+            JButton button = new JButton(element);
+            //Set action command name for button
+            button.setActionCommand(element);
+            //Add listener
+            button.addActionListener(listener);
+            //Add to panel
+            operatorButtonPanel.add(button);
+        }
 
+        //Number button panel
+        JPanel bottomPanel = new JPanel();
+        for(String element:bottomButtons){
+            //Create new button
+            JButton button = new JButton(element);
+            //Set action command name for button
+            button.setActionCommand(element);
+            //Add listener
+            button.addActionListener(listener);
+            //Add to panel
+            bottomPanel.add(button);
+        }
 
-        this.add(displayPanel, BorderLayout.NORTH);
-        this.add(buttonPanel,BorderLayout.CENTER);
-
+        this.add(displayPanel,BorderLayout.NORTH);
+        this.add(numberButtonPanel,BorderLayout.CENTER);
+        this.add(operatorButtonPanel,BorderLayout.EAST);
+        this.add(bottomPanel,BorderLayout.SOUTH);
 
         setVisible(true);
-
-
     }
 }
 
